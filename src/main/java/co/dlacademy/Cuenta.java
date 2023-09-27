772d6f6ef54b5a6e9c17c361e8c80901894a3e96
Alexander Correa Gutierrez <alcogu@utp.edu.co>
2023-09-26 21:01:42 -0500

    se suben cambios

--- src/main/java/co/dlacademy/Cuenta.java
+++ src/main/java/co/dlacademy/Cuenta.java
@@ -1,7 +1,28 @@
 package co.dlacademy;
 
 public class Cuenta {
-    public void transferir(int dinero, Cuenta cuentaDestino){
 
+    private String numeroCuenta;
+
+    public Cuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = String.valueOf(numeroCuenta);
+    }
+
+    public String validarCuentas(int dinero, Cuenta cuentaOrigen, Cuenta cuentaDestino){
+        String cuentasMensaje;
+        if(!(cuentaOrigen.getNumeroCuenta().equals(cuentaDestino.getNumeroCuenta()))){
+            cuentasMensaje = "Cuentas diferentes";
+        }else{
+            cuentasMensaje = "Cuentas Iguales";
+        }
+        return cuentasMensaje;
     }
 }
--- src/test/java/co/dlacademy/CuentaTest.java
+++ src/test/java/co/dlacademy/CuentaTest.java
@@ -1,13 +1,16 @@
 package co.dlacademy;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CuentaTest {
 
     @Test
-    public void testTransferenciaCuentasExitosa(){
-        Cuenta cuentaOrigen = new Cuenta();
-        Cuenta cuentaDestino = new Cuenta();
-        cuentaOrigen.transferir(10000, cuentaDestino);
+    public void testCuentaDestinoDiferente(){
+        Cuenta cuentaOrigen = new Cuenta("1234");
+        Cuenta cuentaDestino = new Cuenta("4321");
+        String resultado = cuentaOrigen.validarCuentas(1000, cuentaOrigen, cuentaDestino);
+
+        Assertions.assertEquals("cuentas diferentes", resultado);
     }
 }
